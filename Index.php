996ae977af5af5addda17c23c90667542812dd43
vishnus272004 <139452983+vishnus272004@users.noreply.github.com>
2023-07-13 16:30:28 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Login Page</title>
+
+<link rel="stylesheet" href="sitestyle.css">
+
+</head>
+
+<body>
+
+<header>
+
+<h1>Cloud Login Demo</h1>
+
+</header>
+
+<hr>
+
+<article>
+
+ <section>
+
+ <h2>Login Details</h1>
+
+ <form action="login.php" method="POST">
+
+ <label>User Name</label>
+
+ <input type="text" name="usr"><br><br>
+
+ <label>Password</label>
+
+ <input type="password" name="pwd"><br>
+
+ <input type="submit" value="Login">
+
+ </form>
+
+ </section>
+
+</article>
+
+<footer>
+
+Developed by K Anbarasan
+
+</footer>
+
+</body>
+</html>
